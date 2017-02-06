d01d86cfafc19e6119ea9622586c36d1c264dd9b
Sagar Mohan <sagarmohan846@gmail.com>
2017-02-06 19:14:06 +0530

    Another fail

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -444,6 +444,9 @@ extern struct cpufreq_governor cpufreq_gov_nightmare;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVEX)
 extern struct cpufreq_governor cpufreq_gov_conservativex;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservativex )
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 /*********************************************************************
